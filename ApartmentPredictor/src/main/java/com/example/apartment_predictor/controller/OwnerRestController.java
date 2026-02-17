88899ae2539534e129154f2bc128c99fab23fd65
tomsanmul <tomsan@gmail.com>
2026-02-17 12:11:45 +0100

    Fix

--- ApartmentPredictor/src/main/java/com/example/apartment_predictor/controller/OwnerRestController.java
+++ ApartmentPredictor/src/main/java/com/example/apartment_predictor/controller/OwnerRestController.java
@@ -1,4 +1,29 @@
 package com.example.apartment_predictor.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.apartment_predictor.model.Owner;
+import com.example.apartment_predictor.repository.OwnerRepository;
+
+@RestController
+@RequestMapping("/api/owners")
 public class OwnerRestController {
+
+    private final OwnerRepository ownerRepo;
+
+    public OwnerRestController(OwnerRepository ownerRepo) {
+        this.ownerRepo = ownerRepo;
+    }
+
+    @GetMapping
+    public List<Owner> getAll() {
+        List<Owner> owners = new ArrayList<>();
+    ownerRepo.findAll().forEach(owners::add);
+    return owners;
+}
 }
