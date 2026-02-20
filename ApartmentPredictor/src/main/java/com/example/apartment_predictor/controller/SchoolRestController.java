a5b12c5ee7ad9896bb614b0b861716efebdcfc42
tomsanmul <tomsan@gmail.com>
2026-02-20 11:35:52 +0100

    fix SchoolRestController

--- ApartmentPredictor/src/main/java/com/example/apartment_predictor/controller/SchoolRestController.java
+++ ApartmentPredictor/src/main/java/com/example/apartment_predictor/controller/SchoolRestController.java
@@ -1,16 +1,19 @@
 package com.example.apartment_predictor.controller;
 
-import java.util.ListteDB;
-wired;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@Requescompexample"apartment_predictor.model.Schoolschool")
+import com.example.apartment_predictor.model.School;
 import com.example.apartment_predictor.utils.PopulateDB;
+
+@RestController
+@RequestMapping("api/school")
 public class SchoolRestController {
 
     @Autowired
@@ -18,10 +21,9 @@ public class SchoolRestController {
 
     @GetMapping("/populate")
     public ResponseEntity<String> populateSchools(@RequestParam int qty) {
-        int qtySchoolsCreated = populateDB.populateSchools(qty);
-        
-        if (qtySchoolsCreated > 0)
-            return ResponseEntity.ok("Populated schools: " + qtySchoolsCreated);
+        List<School> schools = populateDB.populateSchools(qty);
+        if (schools.size() > 0)
+            return ResponseEntity.ok("Populated schools: " + schools.size());
         else
             return ResponseEntity.badRequest().body("Failed to populate schools");
     }
