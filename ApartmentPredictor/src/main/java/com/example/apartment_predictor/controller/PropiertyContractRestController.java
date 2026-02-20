27155a9c42eb81569a392dbf0a2c10fcec2d0faa
tomsanmul <tomsan@gmail.com>
2026-02-20 09:34:35 +0100

    Canvis

--- ApartmentPredictor/src/main/java/com/example/apartment_predictor/controller/PropiertyContractRestController.java
+++
@@ -1,4 +0,0 @@
-package com.example.apartment_predictor.controller;
-
-public class PropiertyContractRestController {
-}
--- ApartmentPredictor/src/main/java/com/example/apartment_predictor/controller/SchoolRestController.java
+++ ApartmentPredictor/src/main/java/com/example/apartment_predictor/controller/SchoolRestController.java
@@ -1,7 +1,7 @@
 package com.example.apartment_predictor.controller;
 
-import com.example.apartment_predictor.utils.PopulateDB;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.ListteDB;
+wired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/school")
+@Requescompexample"apartment_predictor.model.Schoolschool")
+import com.example.apartment_predictor.utils.PopulateDB;
 public class SchoolRestController {
 
     @Autowired
@@ -18,6 +19,7 @@ public class SchoolRestController {
     @GetMapping("/populate")
     public ResponseEntity<String> populateSchools(@RequestParam int qty) {
         int qtySchoolsCreated = populateDB.populateSchools(qty);
+        
         if (qtySchoolsCreated > 0)
             return ResponseEntity.ok("Populated schools: " + qtySchoolsCreated);
         else
