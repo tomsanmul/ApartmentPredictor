d8e0ad3f24b043b10066c60ee23bfad574ac571a
tomsanmul <tomsan@gmail.com>
2026-02-23 12:00:01 +0100

    Canvis Person, Reviewer

--- ApartmentPredictor/src/main/java/com/example/apartment_predictor/model/Person.java
+++ ApartmentPredictor/src/main/java/com/example/apartment_predictor/model/Person.java
@@ -1,48 +1,95 @@
 package com.example.apartment_predictor.model;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import java.time.LocalDate;
+import java.util.UUID;
 
-public class Person {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public  class Person {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-
-    //to do
-    private Long id;
-    private String name;
+    private String id;
+    private String fullName;
+    private LocalDate birthDate;
     private String email;
+    private String password;
+    private boolean isActive;
 
-    //Constructor vacío
-    public Person(){       
+    public Person() {
+        this.id = UUID.randomUUID().toString();
     }
 
-    //Cosntructor
-    public Person(String name, String email) {
-        this.name = name;
+    public Person(String fullName, String email, String password, LocalDate birthDate, boolean isActive) {
+        this.id = UUID.randomUUID().toString();
+        this.fullName = fullName;
         this.email = email;
+        this.password = password;
+        this.birthDate = birthDate;
+        this.isActive = isActive;
     }
 
-    //Getters Setters
-    public String getName() {
-        return name;
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public void setEmail(String email) {
         this.email = email;
     }
-    
-    public Long getId(){ return id;}
-    public void setId(Long id) {this.id = id;} 
 
+    public String getPassword() {
+        return password;
+    }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id='" + id + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", birthDate=" + birthDate +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", isActive=" + isActive +
+                '}';
+    }
 }
