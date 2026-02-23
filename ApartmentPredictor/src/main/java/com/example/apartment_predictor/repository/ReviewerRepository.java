9fdf880634aac13f8297d2cf650c56a329ac2142
tomsanmul <tomsan@gmail.com>
2026-02-23 12:13:18 +0100

    Canvis

--- ApartmentPredictor/src/main/java/com/example/apartment_predictor/repository/ReviewerRepository.java
+++ ApartmentPredictor/src/main/java/com/example/apartment_predictor/repository/ReviewerRepository.java
@@ -1,9 +1,10 @@
 package com.example.apartment_predictor.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
 
 import com.example.apartment_predictor.model.Reviewer;
 
-@Repository
-public interface ReviewerRepository extends JpaRepository<Reviewer, Long> {}
+public interface ReviewerRepository extends CrudRepository<Reviewer, String> {
+
+
+}
