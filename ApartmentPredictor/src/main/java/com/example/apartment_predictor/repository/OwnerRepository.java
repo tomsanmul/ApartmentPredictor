1c3ecd9a08b91eeb679386c7b193e3e679fc7f44
tomsanmul <tomsan@gmail.com>
2026-02-24 13:17:59 +0100

    Canvis OwnerRepository

--- ApartmentPredictor/src/main/java/com/example/apartment_predictor/repository/OwnerRepository.java
+++ ApartmentPredictor/src/main/java/com/example/apartment_predictor/repository/OwnerRepository.java
@@ -1,9 +1,7 @@
 package com.example.apartment_predictor.repository;
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
 import com.example.apartment_predictor.model.Owner;
+import org.springframework.data.repository.CrudRepository;
 
-@Repository
-public interface OwnerRepository extends CrudRepository<Owner, Long> {}
+public interface OwnerRepository extends CrudRepository<Owner, String> {
+}
