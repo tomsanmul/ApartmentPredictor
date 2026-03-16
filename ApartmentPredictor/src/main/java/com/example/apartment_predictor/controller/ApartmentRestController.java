0c31136d76b60cb288f88950fa9842d7dd23f6c8
tomsanmul <tomsan@gmail.com>
2026-03-16 11:36:07 +0100

    Feat @CrossOrigin

--- ApartmentPredictor/src/main/java/com/example/apartment_predictor/controller/ApartmentRestController.java
+++ ApartmentPredictor/src/main/java/com/example/apartment_predictor/controller/ApartmentRestController.java
@@ -1,24 +1,27 @@
 package com.example.apartment_predictor.controller;
 
-import com.example.apartment_predictor.model.Apartment;
-import com.example.apartment_predictor.model.School;
-import com.example.apartment_predictor.repository.SchoolRepository;
-import com.example.apartment_predictor.service.ApartmentService;
-import com.example.apartment_predictor.utils.PopulateDB;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
+import com.example.apartment_predictor.model.Apartment;
+import com.example.apartment_predictor.repository.SchoolRepository;
+import com.example.apartment_predictor.service.ApartmentService;
+import com.example.apartment_predictor.utils.PopulateDB;
 
 @RestController
 @RequestMapping("api/v1/apartment")
+@CrossOrigin(origins = "http://localhost:5173/")
 public class ApartmentRestController {
 
     @Autowired
