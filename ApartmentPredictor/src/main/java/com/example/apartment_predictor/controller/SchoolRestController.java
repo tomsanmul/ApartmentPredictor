9374b17cf869cf6c10f75ab549886f3003623ab7
tomsanmul <tomsan@gmail.com>
2026-04-20 08:56:00 +0200

    Feat SchoolRestController

--- ApartmentPredictor/src/main/java/com/example/apartment_predictor/controller/SchoolRestController.java
+++ ApartmentPredictor/src/main/java/com/example/apartment_predictor/controller/SchoolRestController.java
@@ -3,28 +3,88 @@ package com.example.apartment_predictor.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.apartment_predictor.model.School;
+import com.example.apartment_predictor.repository.SchoolRepository;
 import com.example.apartment_predictor.utils.PopulateDB;
 
 @RestController
-@RequestMapping("api/school")
+@RequestMapping("api/v1/school")
 public class SchoolRestController {
 
     @Autowired
     PopulateDB populateDB;
 
+    @Autowired
+    SchoolRepository schoolRepository;
+
+    @GetMapping("/getAll")
+    public ResponseEntity<Iterable<School>> getAllSchools() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Status", "getAllSchools executed");
+        headers.add("version", "1.0 Api Rest School Object");
+        headers.add("active", "true");
+        headers.add("author", "Albert");
+        
+        return ResponseEntity.ok().headers(headers).body(schoolRepository.findAll());
+    }
+
+    @GetMapping("/getById")
+    public ResponseEntity<School> getSchoolById(@RequestParam String id) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Status", "getSchoolById executed");
+        headers.add("version", "1.0 Api Rest School Object");
+        headers.add("active", "true");
+        headers.add("author", "Albert");
+        
+        School school = schoolRepository.findById(id).orElse(null);
+        return ResponseEntity.ok().headers(headers).body(school);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<School> saveSchool(@RequestBody School school) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Status", "saveSchool executed");
+        headers.add("version", "1.0 Api Rest School Object");
+        headers.add("active", "true");
+        headers.add("author", "Albert");
+        
+        return ResponseEntity.ok().headers(headers).body(schoolRepository.save(school));
+    }
+
     @GetMapping("/populate")
     public ResponseEntity<String> populateSchools(@RequestParam int qty) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Status", "populateSchools executed");
+        headers.add("version", "1.0 Api Rest School Object");
+        headers.add("active", "true");
+        headers.add("author", "Albert");
+        
         List<School> schools = populateDB.populateSchools(qty);
         if (schools.size() > 0)
-            return ResponseEntity.ok("Populated schools: " + schools.size());
+            return ResponseEntity.ok().headers(headers).body("Populated schools: " + schools.size());
         else
-            return ResponseEntity.badRequest().body("Failed to populate schools");
+            return ResponseEntity.badRequest().headers(headers).body("Failed to populate schools");
+    }
+
+    @DeleteMapping("/deleteAll")
+    public ResponseEntity<String> deleteAllSchools() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Status", "deleteAllSchools executed");
+        headers.add("version", "1.0 Api Rest School Object");
+        headers.add("active", "true");
+        headers.add("author", "Albert");
+        
+        schoolRepository.deleteAll();
+        return ResponseEntity.ok().headers(headers).body("All schools deleted");
     }
 }
